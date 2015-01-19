5374c53dd56f555f7bad811e5baf5bb81e3e6cdd
Damowales88 <damien.b.engelhardt@gmail.com>
2015-01-19 14:12:58 -0500

    tested voting

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,9 +1,17 @@
+require 'rails_helper'
+
 describe Vote do
   describe "validations" do
     describe "value validation" do
-      it "only allows -1 or 1 as values" 
-        v = Vote.new
-        expect( v ).to eq(1).or eq(-1)
+      it "only allows -1 or 1 as values" do
+        v = Vote.new(value: 1)
+        expect(v).to be_valid
+        
+        v2 = Vote.new(value: 2)
+        expect(v2).not_to be_valid
+        
+        v3 = Vote.new(value: -1)
+        expect(v3).to be_valid
       end
     end
   end
