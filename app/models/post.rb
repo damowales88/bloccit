cd1fefc40a5d2a3b5e554290b2959ae74e4786c8
Damowales88 <damien.b.engelhardt@gmail.com>
2015-01-24 11:18:55 -0500

    finished voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   has_many :votes, dependent: :destroy
 
+  after_create :create_vote
+
   def up_votes
     votes.where(value: 1).count
   end
@@ -17,7 +19,7 @@ class Post < ActiveRecord::Base
     votes.sum(:value)
   end
 
-  default_scope { order("created_at DESC")}
+  default_scope { order("rank DESC")}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
@@ -39,4 +41,16 @@ class Post < ActiveRecord::Base
     (redcarpet.render text).html_safe
   end
 
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    new_rank = points + age_in_days
+
+    update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
 end
