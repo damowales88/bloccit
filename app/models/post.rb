232787a31aebaee491d40753863d9d8dbda1b610
Damowales88 <damien.b.engelhardt@gmail.com>
2015-01-24 22:48:03 -0500

    crazy testing!

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,6 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   has_many :votes, dependent: :destroy
 
-  after_create :create_vote
-
   def up_votes
     votes.where(value: 1).count
   end
@@ -23,8 +21,8 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-#  validates :user, presence: true
-#  validates :topic, presence: true
+  validates :user, presence: true
+  validates :topic, presence: true
 
   def markdown_title
     render_as_markdown title
@@ -42,13 +40,11 @@ class Post < ActiveRecord::Base
   end
 
   def update_rank
-    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
     new_rank = points + age_in_days
 
     update_attribute(:rank, new_rank)
   end
-
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)
